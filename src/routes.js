23a2e8cce6d50666417d6aa9d74a92bf49cc8361
BrunoSaibert <brunosaibert@gmail.com>
2020-02-02 14:16:28 -0300

    Adicionar ESLint, Prettier e EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Olá Bruno" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Olá Bruno' });
 });
 
 export default routes;
