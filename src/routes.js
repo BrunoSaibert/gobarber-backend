5984da137bd77094a14bd85420a6f44941a485fa
BrunoSaibert <brunosaibert@gmail.com>
2020-02-04 22:45:39 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Olá Bruno' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Bruno Saibert',
+    email: 'brunosaibert@gmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
