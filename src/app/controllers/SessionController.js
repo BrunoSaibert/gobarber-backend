03bc96c0d31e679fc585e404e91c4cb4efcb3b0d
BrunoSaibert <brunosaibert@gmail.com>
2020-02-08 11:33:36 -0300

    Update de usuário && Validando dados de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,23 @@
 import authConfig from '../../config/auth';
 
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -14,7 +27,7 @@ class SessionController {
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not mach.' });
+      return res.status(401).json({ error: 'Password does not match.' });
     }
 
     const { id, name } = user;
