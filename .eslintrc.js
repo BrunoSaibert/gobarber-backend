dad267a2cbc27d39c06ae61942a4dc9332f80065
BrunoSaibert <brunosaibert@gmail.com>
2020-02-12 23:20:29 -0300

    Adicionar Multer

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
-  }
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
 };
